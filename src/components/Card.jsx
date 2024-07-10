8661905ddf1d6cdead333e108514426399c98719
Dhanya Kyei <83703691+Dhanya161@users.noreply.github.com>
2024-07-11 00:45:15 +0800

    I made changes in the dashboard section

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './card.css';
 
 function Card({ card }) {
-  return <div>
-     {card.name}
-    </div>;
+  return (
+    <div className="col-xxl-4 col-md-6 mb-4">
+      <div className="card info-card">
+        <div className="card-body">
+          <h5>{card.name}</h5>
+          <div className="d-flex align-items-center ">
+            <div className="card-icon rounded-circle d-flex align-items-center justify-content-center">
+              <i className={card.icon}></i>
+            </div>
+            <div className="ps-3">
+              <h6>{card.value}</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Card
-
+export default Card;
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './dashboard.css';
 import Card from './Card';
 
 function Dashboard() {
@@ -20,14 +21,14 @@ function Dashboard() {
   return (
   <section className="dashboard section">
     <div className="row">
-      <div className="col-lg-8"></div>
+      <div className="col-lg-8">
           <div className="row">
             {
                cards && cards.length>0 &&
                cards.map(card=><Card key={card._id} card={card} />)
             }
           </div>
-      <div className="col-lg-4"></div>
+          </div>
     </div>
   </section>
   )
